746b443a2ffe68563872e6d8b57b935d480626c5
unknown <m3fezpl@gmail.com>
2021-11-13 14:55:34 +0100

    update

--- src/index.js
+++ src/index.js
@@ -1,95 +1,114 @@
-
 // Pokazywanie i ukrywanie menu
 
 let menuToggle = document.querySelector(".menu-toggle");
 let menu = document.querySelector(".navbar");
 
-{menu.classList.contains("active") ? 
-    menuToggle.classList.remove("fa-bars") : 
-    menuToggle.classList.add("fa-bars")}
+{
+  menu.classList.contains("active")
+    ? menuToggle.classList.remove("fa-bars")
+    : menuToggle.classList.add("fa-bars");
+}
 
-menuToggle.addEventListener("click", ()=>{
-    menu.classList.toggle("active");
+menuToggle.addEventListener("click", () => {
+  menu.classList.toggle("active");
 
-    {menu.classList.contains("active") ? 
-    menuToggle.classList.add("fa-times") : 
-    menuToggle.classList.remove("fa-times")}
-})
+  {
+    menu.classList.contains("active")
+      ? menuToggle.classList.add("fa-times")
+      : menuToggle.classList.remove("fa-times");
+  }
+});
 
-// Obsługa przycisków 
-
+// Obsługa przycisków
 
 let navBtns = document.querySelectorAll(".navbtn");
 let BtnsArray = Array.from(navBtns);
 
-BtnsArray.map(navbtn => {
-    let element = document.querySelector(`.${navbtn.id}`);
-    if(!element){
-        navbtn.classList.add("disabled")
-    }
-    else if(element){
-        navbtn.addEventListener('click', ()=>{
-            element.scrollIntoView({behavior: "smooth"}, true);
-            {menu.classList.contains("active") ? menu.classList.remove("active") : null}
-            {menu.classList.contains("active") ? null : menuToggle.classList.remove("fa-times")}
-    })
-    }
-    
-})
+BtnsArray.map((navbtn) => {
+  let element = document.querySelector(`.${navbtn.id}`);
+  if (!element) {
+    navbtn.classList.add("disabled");
+  } else if (element) {
+    navbtn.addEventListener("click", () => {
+      element.scrollIntoView({ behavior: "smooth" }, true);
+      {
+        menu.classList.contains("active")
+          ? menu.classList.remove("active")
+          : null;
+      }
+      {
+        menu.classList.contains("active")
+          ? null
+          : menuToggle.classList.remove("fa-times");
+      }
+    });
+  }
+});
 
 let arrowBtn = document.querySelector(".arrow-icon");
-arrowBtn.addEventListener("click", ()=>{
-    document.querySelector(".aboutme").scrollIntoView({behavior: "smooth"}, true);
-})
+arrowBtn.addEventListener("click", () => {
+  document
+    .querySelector(".aboutme")
+    .scrollIntoView({ behavior: "smooth" }, true);
+});
 
 // Zamykanie Navbara po kliknięciu poza nim
 
 let sections = document.querySelectorAll("section");
 let sectionsArray = Array.from(sections);
-sectionsArray.map(section => {
-    section.addEventListener("click", ()=>{
-        {menu.classList.contains("active") ? menu.classList.remove("active") : null}
-        {menu.classList.contains("active") ? null : menuToggle.classList.remove("fa-times")}
-    })
-})
+sectionsArray.map((section) => {
+  section.addEventListener("click", () => {
+    {
+      menu.classList.contains("active")
+        ? menu.classList.remove("active")
+        : null;
+    }
+    {
+      menu.classList.contains("active")
+        ? null
+        : menuToggle.classList.remove("fa-times");
+    }
+  });
+});
 
 //Dodanie przycisku powrotu na górę
 
 let scrollUp = document.createElement("div");
 scrollUp.classList.add("scroll-up");
 scrollUp.innerHTML = "<i class='fas fa-chevron-up'></i>";
-scrollUp.addEventListener("click", ()=>{
-    document.body.scrollTop = document.documentElement.scrollTop = 0;
-})
+scrollUp.addEventListener("click", () => {
+  document.body.scrollTop = document.documentElement.scrollTop = 0;
+});
 
-window.addEventListener("scroll", event => {
-    if (scrollY >= document.querySelector(".aboutme").getBoundingClientRect().top * 2){
-        document.body.appendChild(scrollUp);
-        menuToggle.classList.add("scrolled");
-    }
-    else {
-        scrollUp.remove();
-        menuToggle.classList.remove("scrolled");
-    }
+window.addEventListener("scroll", (event) => {
+  if (
+    scrollY >=
+    document.querySelector(".aboutme").getBoundingClientRect().top * 2
+  ) {
+    document.body.appendChild(scrollUp);
+    menuToggle.classList.add("scrolled");
+  } else {
+    scrollUp.remove();
+    menuToggle.classList.remove("scrolled");
+  }
+});
 
-})
+//Wiadomość
 
-window.addEventListener("DOMContentLoaded", event =>{
-    document.querySelector(".close").addEventListener("click", ()=>{
-        document.querySelector(".message").classList.add("remove");
-        setTimeout(()=>{
-            document.querySelector(".message").remove();
-        }, 1000)
-    })
+// window.addEventListener("DOMContentLoaded", event =>{
+//     document.querySelector(".close").addEventListener("click", ()=>{
+//         document.querySelector(".message").classList.add("remove");
+//         setTimeout(()=>{
+//             document.querySelector(".message").remove();
+//         }, 1000)
+//     })
 
-        setTimeout(()=>{
-            if(document.querySelector(".message")){
-                document.querySelector(".message").classList.add("remove");
-                setTimeout(()=>{
-                    document.querySelector(".message").remove();
-                }, 1000)
-            }
-        }, 7000);
-})
-
-
+//         setTimeout(()=>{
+//             if(document.querySelector(".message")){
+//                 document.querySelector(".message").classList.add("remove");
+//                 setTimeout(()=>{
+//                     document.querySelector(".message").remove();
+//                 }, 1000)
+//             }
+//         }, 7000);
+// })
